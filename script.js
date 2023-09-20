3fac0a0f668c6cf4693aafee7f432683625cbc8a
blainefreestone <blainefreestone01@gmail.com>
2023-09-20 15:22:19 -0700

    Display is now updated correctly based on button input. Operations cannot be stringed together quite yet.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let operator;
 let firstNumber;
 let secondNumber;
+let displayValue = "";
+
+buttons = document.querySelectorAll('button');
+display = document.querySelector('#display');
 
 function add(a,b) {return a + b};
 function subtract(a,b) {return a - b};
@@ -17,7 +21,7 @@ function operate(operator, firstNumber, secondNumber) {
         case '-':
             evaluation = subtract(firstNumber, secondNumber);
             break;
-        case '*':
+        case 'x':
             evaluation = multiply(firstNumber, secondNumber);
             break;
         case '/':
@@ -26,4 +30,31 @@ function operate(operator, firstNumber, secondNumber) {
     }
 
     return evaluation;
-}
+};
+
+function updateDisplayValue(e) {
+    btnValue = e.target.getAttribute('data-value');
+    if (['1','2','3','4','5','6','7','8','9','0'].includes(btnValue)) {
+        displayValue += btnValue;
+        display.textContent = displayValue;
+    }
+    else if (['+','-','x','/'].includes(btnValue)) {
+        firstNumber = displayValue * 1;
+        operator = btnValue;
+        displayValue = '';
+    }
+    else if (btnValue === '=') {
+        secondNumber = displayValue * 1;
+        displayValue = operate(operator, firstNumber, secondNumber);
+        display.textContent = displayValue;
+    }
+    else if (btnValue === 'c') {
+        firstNumber = null;
+        secondNumber = null;
+        operator = null;
+        displayValue = "";
+        display.textContent = displayValue;
+    };
+};
+
+buttons.forEach(btn => btn.addEventListener('click', updateDisplayValue));
